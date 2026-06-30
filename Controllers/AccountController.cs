ba0c870413e4848d8fab97b3f42fbf9f845aa2ac
AmmarNassar4 <ammar.nassar@outlook.com>
2026-06-30 12:45:38 +0300

    Add explicit account controller dependency usings

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -4,7 +4,9 @@ using HotelRoomsWeb.Services;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace HotelRoomsWeb.Controllers
 {
